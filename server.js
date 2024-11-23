25d01e91e6ed0dc1784d5a1b29054f5cf4ed1aee
Madam Vignesh <madamvignesh@gmail.com>
2024-11-23 12:06:58 +0530

    deploying backend

---
+++ server.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const { open } = require('sqlite');
+const cors = require('cors')
+const sqlite3 = require('sqlite3');
+const path = require('path');
+
+const app = express();
+app.use(cors()); 
+app.use(express.json());
+
+const dbPath = path.join(__dirname, 'transactionManagement.db');
+let db = null;
+
+// Initialize DB and Server
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(8081, () => {
+      console.log(`Server Running at http://localhost:8081/`);
+    });
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+initializeDbAndServer();
+
+
+app.get('/api/transactions/', async (req, res) => {
+  const { user_id } = req.query; // Get query parameter
+  let query;
+  let params = [];
+
+  if (user_id) {
+    query = `
+      SELECT transaction_id, user_id, amount, transaction_type, time AS timestamp, status
+      FROM transactions
+      WHERE user_id = ?
+    `;
+    params = [user_id];
+  } else {
+    query = `
+      SELECT transaction_id, user_id, amount, transaction_type, time AS timestamp, status
+      FROM transactions
+    `;
+  }
+  try {
+    const transactions = await db.all(query, params);
+    res.json(transactions); 
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch transactions', message: error.message });
+  }
+});
+
+// To get specific transaction 
+app.get('/api/transactions/:transactionID', async (req, res) => {
+  const { transactionID } = req.params;
+  const query = `
+    SELECT transaction_id, user_id, amount, transaction_type, time AS timestamp, status
+    FROM transactions
+    WHERE transaction_id = ?`;
+  try {
+    const transaction = await db.get(query, [transactionID]);
+    if (transaction) {
+      res.json(transaction);
+    } else {
+      res.status(404).json({ message: 'Transaction not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch transaction', message: error.message });
+  }
+});
+
+// To Create Transaction 
+app.post('/api/transactions', async (req, res) => {
+  const { transaction_id, user_id, amount, transaction_type, timestamp, status } = req.body;
+  const query = `
+    INSERT INTO transactions (transaction_id, user_id, amount, transaction_type, time, status)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
+  try {
+    await db.run(query, [transaction_id, user_id, amount, transaction_type, timestamp, status]);
+    res.status(201).json({ message: 'Transaction created successfully' });
+  } catch (error) {
+    console.error('POST Error:', error.message);
+    res.status(500).json({ error: 'Failed to create transaction', details: error.message });
+  }
+});
+
+// API to update a transaction's status
+app.put('/api/transactions/:transactionID', async (req, res) => {
+  const { transactionID } = req.params;
+  const { status } = req.body;
+  const query = `
+    UPDATE transactions
+    SET status = ?
+    WHERE transaction_id = ?`;
+  try {
+    const result = await db.run(query, [status, transactionID]);
+    if (result.changes > 0) {
+      res.json({ message: 'Transaction status updated successfully', status });
+    } else {
+      res.status(404).json({ message: 'Transaction not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update transaction status', message: error.message });
+  }
+});
+
+// Root endpoint
+app.get('/', (req, res) => {
+  res.json('Server is connecting...');
+});
+
+module.exports = app;
---
+++ transactionmangament.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2024 at 04:49 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `transactionmangament`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `transaction_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `transaction_type` varchar(20) NOT NULL,
+  `time` date NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`transaction_id`, `user_id`, `amount`, `transaction_type`, `time`, `status`) VALUES
+(0, 12, 5600, 'DEPOSIT', '2023-05-17', 'FAILED'),
+(322958, 3, 400, 'DEPOSIT', '2024-11-21', 'COMPLETED'),
+(51522828, 452, 7000, 'WITHDRAWAL', '2024-11-21', 'PENDING');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`transaction_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
